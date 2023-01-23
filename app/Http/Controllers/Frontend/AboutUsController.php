06bc88e2d3d8d3ba991165e7af5151a45d8799e5
Faisal DL <Faisal@dynamologic.com>
2023-01-23 22:23:47 +0500

    update code

--- app/Http/Controllers/Frontend/AboutUsController.php
+++ app/Http/Controllers/Frontend/AboutUsController.php
@@ -8,11 +8,26 @@ use Illuminate\Http\Request;
 
 class AboutUsController extends Controller
 {
- public function aboutUs()
- {
+    public function aboutUs()
+    {
 
-     $this->pageData["page_title"] = "About Us";
-     return $this->showPage("front_end.about_us");
+//        $text = 'My tailor is rich and Alison is in the kitchen with Bob.';
+        $text = 'For  آپ کسی ہے  example, typing آپ کسی ہے  "Aap kasai hai?" becomes "آپ کسی ہے ?".';
+        $text = 'آپ کسی ہےآپ کسی ہےآپ کسی ہےآپ کسی ہےآپ کسی ہےآپ کسی ہےآپ  کسی ہےآپ کسی ہےآپ کسی ہے ?my name is ';
 
- }
+        // $detector = new LanguageDetector(null, ['en', 'fr', 'de']);
+
+        $detector = new \LanguageDetector\LanguageDetector(null, ['en', 'ur']);
+        $language = $detector->evaluate($text)->getScores();
+        dd($language);
+
+        // $language = $detector->evaluate($text)->getLanguage();
+        echo $language; // Prints something like 'en'
+        exit;
+
+
+        $this->pageData["page_title"] = "About Us";
+        return $this->showPage("front_end.about_us");
+
+    }
 }
